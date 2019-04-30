c4c857a9ba1eb2905703b37b8e83ee7ad4aedd83
Aneury <aneury1@gmail.com>
2019-04-29 23:06:35 -0400

    adding gnu multiboot.h file

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,7 +2,10 @@
 
 #include <kernel/tty.h>
 
-void kernel_main(void) {
+
+
+void kernel_main(void) 
+{
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+	printf("Annette OS 2016-2019 Aneury perez.\n");
 }
--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -5,8 +5,6 @@
 #include <string.h>
 
 
-
-
 static bool print(const char* data, size_t length) {
 	const unsigned char* bytes = (const unsigned char*) data;
 	for (size_t i = 0; i < length; i++)
