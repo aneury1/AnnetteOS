de2fff10e1d43975d4a1565f2f9b1f1cfc6ba2e2
aneury1 <aneury1@gmail.com>
2019-12-15 08:22:35 -0400

    add gdt header

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,9 +1,23 @@
 #include "gnu/gtypes.h"
 #include "kernel/vconsole.h"
 
+#ifdef __INTEL_CPU
+#include "hardware/x86/GDT.h"
+static void init_hw(){
+
+}
+#else
+#warning "by now only intel would be supported"
+static void init_hw(){
+   kprintf("\nthis hardware is not yet supported");  
+}
+#endif
+
+
 extern "C"
 void kernel_main(void) 
 {
    kcls();
    kprintf("AnnetteOS V0.0.01b");
+   init_hw();
 }
